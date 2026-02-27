1ad8f360d120510f5d12eacca2dc8c64f1c7e45a
Munirmahedi Paviwala <munir1612@gmail.com>
2026-02-27 12:40:56 -0500

    added new stuff

--- js/gallery-data.js
+++ js/gallery-data.js
@@ -5,7 +5,7 @@ const galleryData = [
         artist: "Munir Paviwala",
         date: "Feb 26, 2026",
         image: "assets/key_a.gif",
-        note: "This is my first attempt at any pixel art. I'm using a 32x32 canvas. I chose to draw a key from the mechanical keyboard that is in front of me."
+        note: "My first attempt at any pixel art. I used a 32x32 canvas. I chose to draw a key from the mechanical keyboard that was in front of me."
     },
     {
         id: 2,
@@ -13,7 +13,7 @@ const galleryData = [
         artist: "Munir Paviwala",
         date: "Feb 26, 2026",
         image: "assets/ein_blinking.gif",
-        note: "Ein is a Pembroke Welsh Corgi from the anime series Cowboy Bebop. He is known for his intelligence, loyalty, and his ability to communicate with humans through a telepathic translator collar. I wanted it to feel like being patted."
+        note: "Ein is a pet from the anime series Cowboy Bebop. He is known for his intelligence, loyalty, and his ability to communicate with humans through a telepathic translator collar. I wanted it to feel like being patted."
     },
     {
         id: 3,
@@ -23,4 +23,21 @@ const galleryData = [
         image: "assets/blink-battery.gif",
         note: "Just a battery charging animation."
     },
+    {
+        id: 4,
+        title: "Night sky Rainbow",
+        artist: "Munir Paviwala",
+        date: "Feb 27, 2026",
+        image: "assets/night-sky-rainbow.gif",
+        note: "Just a night sky rainbow - with some sparkles and blinks. But more as a re-imagination of a static I saw on Pinterest."
+    },
+    {
+        id: 5,
+        title: "Sunset at Industrial Area",
+        artist: "Munir Paviwala",
+        date: "Feb 27, 2026",
+        image: "assets/industrial-sunset-pixilart.png",
+        note: "A larger canvas - 320 x 180. I wanted to try making more atmospheric art. I guess, this was okay for a first attempt."
+    }
+
 ];
